370e7472993ac9fa20ba6c39f0b03aabad040cfc
Matthew J. Schultz <45434369+matthewjschultz@users.noreply.github.com>
2026-06-17 21:11:14 -0700

    mjsfx: add cask v1.0

---
+++ Casks/mjsfx.rb
@@ -0,0 +1,19 @@
+cask "mjsfx" do
+  version "1.0"
+  sha256 "db46f82b34687501af585d8b72d382c0d454de1cd152b04fc3af3014920e3d18"
+
+  url "https://mjsfx.app/releases/MJSFX-#{version}.dmg"
+  name "MJSFX"
+  desc "Native macOS designer for 8-bit retro game SFX"
+  homepage "https://mjsfx.app"
+
+  auto_updates true
+
+  app "MJSFX.app"
+
+  zap trash: [
+    "~/Library/Preferences/dev.mjs.app.mjsfx.plist",
+    "~/Library/Saved Application State/dev.mjs.app.mjsfx.savedState",
+    "~/Library/Application Support/MJSFX",
+  ]
+end
