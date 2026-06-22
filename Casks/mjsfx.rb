390bb723fb1ae50aba5afcc67929bc40d1bc9c07
Matthew J. Schultz <45434369+matthewjschultz@users.noreply.github.com>
2026-06-22 08:59:24 -0400

    mjsfx: update to 1.2

--- Casks/mjsfx.rb
+++ Casks/mjsfx.rb
@@ -1,6 +1,6 @@
 cask "mjsfx" do
-  version "1.1"
-  sha256 "c9655cdc862a795933ee98caa37343b2fef58367040b9a29c65e68f3576cec21"
+  version "1.2"
+  sha256 "f121c2e2c3779d61e39dd07437014b2fb81dc9369b7645bec573bbc1a6c07679"
 
   url "https://mjsfx.app/releases/MJSFX-#{version}.dmg"
   name "MJSFX"
