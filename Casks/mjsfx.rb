3f336b977eb24a9e8bb932b0d0c40fe55d906267
Matthew J. Schultz <45434369+matthewjschultz@users.noreply.github.com>
2026-06-28 14:37:14 -0400

    mjsfx: update to 1.9

--- Casks/mjsfx.rb
+++ Casks/mjsfx.rb
@@ -1,6 +1,6 @@
 cask "mjsfx" do
-  version "1.8"
-  sha256 "0e5f1d2d296970e6a94575221319f74efb75ac31e9e67791b91b32a21f9adef6"
+  version "1.9"
+  sha256 "9eb6b7dfafe50efefb921f4d2e0aaece18f5be641eb9d22fd47d1c634534c8c6"
 
   url "https://mjsfx.app/releases/MJSFX-#{version}.dmg"
   name "MJSFX"
